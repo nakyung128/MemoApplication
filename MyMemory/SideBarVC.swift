a787d54235d1683f30ad0e28e2b29c92509a04ab
nakyung128 <lnk12888@naver.com>
2023-04-03 02:10:54 +0900

    Add: 로그인, 로그아웃, 프로필 사진 변경

--- MyMemory/SideBarVC.swift
+++ MyMemory/SideBarVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class SideBarVC: UITableViewController {
+    let uinfo = UserInfoManager() // 개인정보 관리 매니저
+    
     // 목록 데이터 배열
     let titles = [ "새 글 작성하기", "친구 새 글", "달력으로 보기", "공지사항", "통계", "계정 관리" ]
     
@@ -72,7 +74,6 @@ class SideBarVC: UITableViewController {
         
         // 이름 레이블의 속성을 정의하고 헤더 뷰에 추가한다.
         self.nameLabel.frame = CGRect(x: 70, y: 15, width: 100, height: 30) // 위치와 크기를 정의
-        self.nameLabel.text = "나경의 메모장" // 기본 텍스트
         self.nameLabel.textColor = UIColor.white // 텍스트 색상
         self.nameLabel.font = UIFont.boldSystemFont(ofSize: 15) // 폰트 사이즈
         self.nameLabel.backgroundColor = UIColor.clear // 배경 색상 (투명)
@@ -81,7 +82,6 @@ class SideBarVC: UITableViewController {
         
         // 이메일 레이블의 속성을 정의하고 헤더 뷰에 추가한다.
         self.emailLabel.frame = CGRect(x: 70, y: 30, width: 130, height: 30)
-        self.emailLabel.text = "abc123@google.com" // 기본 텍스트
         self.emailLabel.textColor = UIColor.white
         self.emailLabel.font = UIFont.systemFont(ofSize: 12)
         self.emailLabel.backgroundColor = UIColor.clear
@@ -89,8 +89,6 @@ class SideBarVC: UITableViewController {
         headerView.addSubview(self.emailLabel)
         
         // 기본 이미지를 구현한다.
-        let defaultProfile = UIImage(named: "account.jpg")
-        self.profileImage.image = defaultProfile // 이미지 등록
         self.profileImage.frame = CGRect(x: 10, y: 10, width: 50, height: 50)
         
         // 프로필 이미지 둥글게 만들기
@@ -99,5 +97,11 @@ class SideBarVC: UITableViewController {
         self.profileImage.layer.masksToBounds = true // 마스크 효과
         
         view.addSubview(self.profileImage)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.nameLabel.text = self.uinfo.name ?? "Guest"
+        self.emailLabel.text = self.uinfo.account ?? ""
+        self.profileImage.image = self.uinfo.profile
     }
 }
